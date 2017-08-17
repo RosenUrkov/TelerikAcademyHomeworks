53ab0cae6c221c7528afa2f50fccb8805d093af1
Rosen Urkov <rosen.urkov@gmail.com>
2017-08-17 17:43:32 +0300

    Added Aho-Corasick algorithm implementation

---
+++ DSA/StringAlgorithms/AhoCorasick/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AhoCorasick
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var strings = new string[]
+                {
+                    "a",
+                    "b",
+                    "ab",
+                    "bab",
+                    "bc",
+                    "bca",
+                    "c",
+                    "caa"
+                };
+
+            var root = new Trie();
+            foreach (var str in strings)
+            {
+                root.AddString(str);
+            }
+
+            root.Precompute();
+
+            var text = "asdbbasbcbascbabcbabcsabc";
+            root.AhoCorasick(text);
+        }
+    }
+}
