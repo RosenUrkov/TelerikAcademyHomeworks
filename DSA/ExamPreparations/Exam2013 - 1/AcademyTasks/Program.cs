2bb3ae3b920cfdfe64968a5494978f8ff347fdf9
Rosen Urkov <rosen.urkov@gmail.com>
2017-08-18 23:16:01 +0300

    Added some tasks and exam preps for DSA course

---
+++ DSA/ExamPreparations/Exam2013 - 1/AcademyTasks/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyTasks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var coef = int.Parse(Console.ReadLine());
+
+            int minTaskValue = list[0];
+            int maxTaskValue = list[0];
+
+            int result = 0;
+            var isEnded = false;
+            for (int i = 0; i < list.Count; i++)
+            {
+                result++;
+
+                if (list[i] < minTaskValue)
+                {
+                    minTaskValue = list[i];
+                }
+                if (list[i] > maxTaskValue)
+                {
+                    maxTaskValue = list[i];
+                }
+
+                if (maxTaskValue - minTaskValue >= coef)
+                {
+                    isEnded = true;
+                    Console.WriteLine(result);
+                    break;
+                }
+
+                if (i + 1 < list.Count &&
+                    (list[i + 1] - minTaskValue >= coef ||
+                    maxTaskValue - list[i + 1] >= coef))
+                {
+                    continue;
+                }
+                else if (i + 2 < list.Count &&
+                    ((list[i + 2] - minTaskValue < coef &&
+                    list[i + 2] - list[i + 1] >= coef) ||
+                    (maxTaskValue - list[i + 2] < coef &&
+                    list[i + 1] - list[i + 2] >= coef)))
+                {
+                    continue;
+                }
+                else if (i + 3 < list.Count &&
+                    ((list[i + 3] - list[i + 2] < coef &&
+                    list[i + 3] - list[i + 1] >= coef) ||
+                    (list[i + 2] - list[i + 3] < coef &&
+                    list[i + 1] - list[i + 3] >= coef)))
+                {
+                    continue;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            if (!isEnded)
+            {
+                Console.WriteLine(list.Count);
+            }
+        }
+    }
+}
---
+++ DSA/ExamPreparations/Exam2013 - 1/MessagesInABottle/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MessagesInABottle
+{
+    public class Program
+    {
+        public static Dictionary<string, char> cypher;
+        public static SortedSet<string> result;
+
+        public static void Main(string[] args)
+        {
+            var message = Console.ReadLine();
+            cypher = new Dictionary<string, char>();
+
+            var matches = Regex.Matches(Console.ReadLine(), "[A-Z][0-9]+");
+            foreach (var item in matches)
+            {
+                var match = item as Match;
+                cypher.Add(match.Value.Substring(1), match.Value[0]);
+            }
+
+            result = new SortedSet<string>();
+            Recursion(0, message);
+
+            Console.WriteLine(result.Count);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Recursion(int index, string msg)
+        {
+            var decyphered = true;
+            for (int i = 0; i < msg.Length; i++)
+            {
+                if (char.IsDigit(msg[i]))
+                {
+                    decyphered = false;
+                    break;
+                }
+            }
+
+            if (decyphered)
+            {
+                result.Add(msg);
+                return;
+            }
+
+            var newMsg = string.Empty;
+            string key = string.Empty;
+            for (int i = index; i < msg.Length; i++)
+            {
+                key += msg[i];
+                if (TryDecypher(msg, key, index, out newMsg))
+                {
+                    Recursion(index + 1, newMsg);
+                }
+            }
+        }
+
+        public static bool TryDecypher(string msg, string charCypher, int index, out string result)
+        {
+            if (cypher.ContainsKey(charCypher))
+            {
+                result = msg.Substring(0, index) + cypher[charCypher] + msg.Substring(index + charCypher.Length);
+                return true;
+            }
+
+            result = msg;
+            return false;
+        }
+    }
+}
