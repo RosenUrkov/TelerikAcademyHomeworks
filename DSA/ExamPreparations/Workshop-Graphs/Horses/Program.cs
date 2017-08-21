9c884aae82ebe60848ee975a3b2c91e96cf086e0
Rosen Urkov <rosen.urkov@gmail.com>
2017-08-21 17:58:11 +0300

    Added graphs workshop

---
+++ DSA/ExamPreparations/Workshop-Graphs/Horses/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horses
+{
+    public class Program
+    {
+        //public static int optimalResult = int.MaxValue;
+
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var matrix = new string[n, n];
+            var visited = new bool[n, n];
+
+            Position startCoords = null;
+            Position endCoords = null;
+
+            for (int row = 0; row < n; row++)
+            {
+                var rowValues = Console.ReadLine().Split(' ');
+                for (int col = 0; col < n; col++)
+                {
+                    if (rowValues[col] == "x")
+                    {
+                        visited[row, col] = true;
+                    }
+                    if (rowValues[col] == "s")
+                    {
+                        startCoords = new Position(row, col, 0);
+                    }
+                    if (rowValues[col] == "e")
+                    {
+                        endCoords = new Position(row, col, 0);
+                    }
+
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            var possibleRows = new int[] { 2, 2, -2, -2, 1, 1, -1, -1 };
+            var possibleCols = new int[] { 1, -1, 1, -1, 2, -2, 2, -2 };
+
+            var queue = new Queue<Position>();
+            queue.Enqueue(startCoords);
+            visited[startCoords.Item1, startCoords.Item2] = true;
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                for (int i = 0; i < possibleCols.Length; i++)
+                {
+                    if (current.Item1 + possibleRows[i] < 0 ||
+                        current.Item1 + possibleRows[i] >= matrix.GetLength(0) ||
+                        current.Item2 + possibleCols[i] < 0 ||
+                        current.Item2 + possibleCols[i] >= matrix.GetLength(1) ||
+                        visited[current.Item1 + possibleRows[i], current.Item2 + possibleCols[i]])
+                    {
+                        continue;
+                    }
+
+                    if (current.Item1 + possibleRows[i] == endCoords.Item1 &&
+                        current.Item2 + possibleCols[i] == endCoords.Item2)
+                    {
+                        Console.WriteLine(current.Item3 + 1);
+                        return;
+                    }
+
+                    visited[current.Item1 + possibleRows[i], current.Item2 + possibleCols[i]] = true;
+                    queue.Enqueue(new Position(current.Item1 + possibleRows[i], current.Item2 + possibleCols[i], current.Item3 + 1));
+                }
+            }
+
+            Console.WriteLine("No");
+        }
+    }
+
+    class Position
+    {
+        public Position(int Item1, int Item2, int Item3)
+        {
+            this.Item1 = Item1;
+            this.Item2 = Item2;
+            this.Item3 = Item3;
+        }
+
+        public int Item1 { get; set; }
+
+        public int Item2 { get; set; }
+
+        public int Item3 { get; set; }
+    }
+}
---
+++ DSA/ExamPreparations/Workshop-Graphs/HungryTom/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HungryTom
+{
+    public class Program
+    {
+        public static SortedSet<string> results = new SortedSet<string>();
+
+        public static void Main(string[] args)
+        {
+            var roomsCount = int.Parse(Console.ReadLine());
+            var doorsCount = int.Parse(Console.ReadLine());
+
+            if (roomsCount < 1 || doorsCount < 1)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+
+            var graph = ReadNonWeightedGraph(roomsCount, doorsCount);
+            var visited = new HashSet<int>();
+
+            DFS(1, 1, graph, visited, "");
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine(-1);
+                return;
+            }
+
+            Console.WriteLine(results.Count);
+            foreach (var item in results)
+            {
+                var corrcet = item
+                    .Replace("9", "ten")
+                    .Replace("8", "9")
+                    .Replace("7", "8")
+                    .Replace("6", "7")
+                    .Replace("5", "6")
+                    .Replace("4", "5")
+                    .Replace("3", "4")
+                    .Replace("2", "3")
+                    .Replace("1", "2")
+                    .Replace("0", "1")
+                    .Replace("ten", "10");
+
+                Console.WriteLine(corrcet);
+            }
+        }
+
+        private static List<int>[] ReadNonWeightedGraph(int nodesCount, int edgesCount)
+        {
+            var vertices = new List<int>[nodesCount + 1];
+
+            var current = new List<int[]>();
+            for (int i = 0; i < edgesCount; i++)
+            {
+                current.Add(Console.ReadLine().Split(' ').Select(int.Parse).ToArray());
+            }
+
+            current.ForEach(x =>
+            {
+                var from = x[0];
+                var to = x[1];
+
+                if (vertices[from] == null)
+                {
+                    vertices[from] = new List<int>();
+                }
+                vertices[from].Add(to);
+
+                if (vertices[to] == null)
+                {
+                    vertices[to] = new List<int>();
+                }
+                vertices[to].Add(from);
+            });
+
+            return vertices;
+        }
+
+        private static void DFS(int currVertex, int startVertex, List<int>[] vertices, HashSet<int> visited, string result)
+        {
+            if (visited.Count == vertices.Length - 2)
+            {
+                if (vertices[currVertex].Contains(startVertex))
+                {
+                    results.Add(result + (currVertex - 1) + " " + (startVertex - 1));
+                }
+
+                return;
+            }
+
+            visited.Add(currVertex);
+
+            vertices[currVertex].ForEach(x =>
+            {
+                if (!visited.Contains(x))
+                {
+                    DFS(x, startVertex, vertices, visited, result + (currVertex - 1) + " ");
+                }
+            });
+
+            visited.Remove(currVertex);
+        }
+    }
+}
