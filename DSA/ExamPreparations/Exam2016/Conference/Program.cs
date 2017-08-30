024eb31479d75a7adc239761ebae5088167faadf
Rosen Urkov <rosen.urkov@gmail.com>
2017-08-30 15:29:34 +0300

    Added Exam2016 part 2

--- DSA/ExamPreparations/Exam2016/Conference/Program.cs
+++ DSA/ExamPreparations/Exam2016/Conference/Program.cs
@@ -10,22 +10,39 @@
             var line = Console.ReadLine().Split(' ');
             int nodesCount = int.Parse(line[0]);
             int edgesCount = int.Parse(line[1]);
-            List<Edge> edges = new List<Edge>();
 
+            List<Edge> edges = new List<Edge>();
             InitializeGraph(edges, edgesCount);
 
-            edges.Sort();
-
-            int[] tree = new int[nodesCount + 1]; //we start from 1, not from 0
-            var mpd = new List<Edge>();
+            int[] tree = new int[nodesCount]; //we start from 1, not from 0
             int treesCount = 1;
 
-            treesCount = FindMinimumSpanningTree(edges, tree, mpd, treesCount);
+            FindMinimumSpanningTree(edges, tree, treesCount);
 
-            PrintMinimumSpanningTree(mpd);
+            var result = new int[nodesCount];
+            for (int i = 0; i < tree.Length; i++)
+            {
+                result[tree[i]]++;
+            }
+
+            long sum = 0;
+            for (int i = nodesCount - 1; i > 0; i--)
+            {
+                sum += result[i] * (nodesCount - result[i]);
+                nodesCount -= result[i];
+            }
+
+            long noFactorial = 0;
+            for (int i = result[0] - 1; i > 0; i--)
+            {
+                noFactorial += i;
+            }
+
+            sum += noFactorial;
+            Console.WriteLine(sum);
         }
 
-        private static int FindMinimumSpanningTree(List<Edge> edges, int[] tree, List<Edge> mpd, int treesCount)
+        private static int FindMinimumSpanningTree(List<Edge> edges, int[] tree, int treesCount)
         {
             foreach (var edge in edges)
             {
@@ -41,7 +58,6 @@
                         // attach the start node to the tree of the end node
                         tree[edge.StartNode] = tree[edge.EndNode];
                     }
-                    mpd.Add(edge);
                 }
                 else // the start is part of a tree
                 {
@@ -49,7 +65,6 @@
                     {
                         //attach the end node to the tree;
                         tree[edge.EndNode] = tree[edge.StartNode];
-                        mpd.Add(edge);
                     }
                     else if (tree[edge.EndNode] != tree[edge.StartNode]) // combine the trees
                     {
@@ -62,19 +77,10 @@
                                 tree[i] = tree[edge.StartNode];
                             }
                         }
-                        mpd.Add(edge);
                     }
                 }
             }
             return treesCount;
-        }
-
-        private static void PrintMinimumSpanningTree(IEnumerable<Edge> usedEdges)
-        {
-            foreach (var edge in usedEdges)
-            {
-                Console.WriteLine(edge);
-            }
         }
 
         private static void InitializeGraph(List<Edge> edges, int edgesCount)
@@ -82,40 +88,21 @@
             for (int i = 0; i < edgesCount; i++)
             {
                 var line = Console.ReadLine().Split(' ');
-                edges.Add(new Edge(int.Parse(line[0]), int.Parse(line[1]), int.Parse(line[2])));
+                edges.Add(new Edge(int.Parse(line[0]), int.Parse(line[1])));
             }
         }
     }
 
-    public class Edge : IComparable<Edge>
+    public class Edge
     {
-        public Edge(int startNode, int endNode, int weight)
+        public Edge(int startNode, int endNode)
         {
             this.StartNode = startNode;
             this.EndNode = endNode;
-            this.Weight = weight;
         }
 
         public int StartNode { get; set; }
 
         public int EndNode { get; set; }
-
-        public int Weight { get; set; }
-
-        public int CompareTo(Edge other)
-        {
-            int weightCompared = this.Weight.CompareTo(other.Weight);
-            if (weightCompared == 0)
-            {
-                return this.StartNode.CompareTo(other.StartNode);
-            }
-
-            return weightCompared;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("({0} {1}) -> {2}", this.StartNode, this.EndNode, this.Weight);
-        }
     }
 }
---
+++ DSA/ExamPreparations/Exam2016/Guards/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rowsAndCols = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var matrix = new int[rowsAndCols[0] + 1, rowsAndCols[1] + 1];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, 0] = -1;
+            }
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                matrix[0, i] = -1;
+            }
+
+            matrix[0, 1] = 0;
+
+            var guards = int.Parse(Console.ReadLine());
+            for (int i = 0; i < guards; i++)
+            {
+                var parameters = Console.ReadLine().Split(' ');
+
+                var row = int.Parse(parameters[0]) + 1;
+                var col = int.Parse(parameters[1]) + 1;
+
+                matrix[row, col] = -1;
+
+                var guardedRow = row;
+                var guardedCol = col;
+                switch (parameters[2])
+                {
+                    case "R": guardedCol++; break;
+                    case "D": guardedRow++; break;
+                    case "L": guardedCol--; break;
+                    case "U": guardedRow--; break;
+                }
+
+                if (guardedRow < 0 ||
+                    guardedRow >= matrix.GetLength(0) ||
+                    guardedCol < 0 ||
+                    guardedCol >= matrix.GetLength(1))
+                {
+                    continue;
+                }
+
+                matrix[guardedRow, guardedCol] = matrix[guardedRow, guardedCol] == -1 ? -1 : 2;
+            }
+
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == -1 || (matrix[row - 1, col] == -1 && matrix[row, col - 1] == -1))
+                    {
+                        matrix[row, col] = -1;
+                        continue;
+                    }
+
+                    if (matrix[row - 1, col] == -1)
+                    {
+                        matrix[row, col] += matrix[row, col - 1] + 1;
+                    }
+                    else if (matrix[row, col - 1] == -1)
+                    {
+                        matrix[row, col] += matrix[row - 1, col] + 1;
+                    }
+                    else
+                    {
+                        matrix[row, col] += Math.Min(matrix[row - 1, col], matrix[row, col - 1]) + 1;
+                    }
+                }
+            }
+
+            var result = matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
+            if (result == -1)
+            {
+                Console.WriteLine("Meow");
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
