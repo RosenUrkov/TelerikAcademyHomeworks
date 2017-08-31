b32643806334fa7bfda51b3952fc0e2fd6adf8e6
Rosen Urkov <rosen.urkov@gmail.com>
2017-08-31 22:06:47 +0300

    Added some DSA 2015 exam tasks

--- DSA/ExamPreparations/Exam2015/ABoxFullOfBalls/Program.cs
+++ DSA/ExamPreparations/Exam2015/ABoxFullOfBalls/Program.cs
@@ -10,6 +10,42 @@ namespace ABoxFullOfBalls
     {
         static void Main(string[] args)
         {
+            var moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            var startAndEnd = Console.ReadLine().Split(' ');
+            var start = int.Parse(startAndEnd[0]);
+            var end = int.Parse(startAndEnd[1]) + 1;
+
+            var result = new bool[end];
+            for (int i = 0; i < moves.Length; i++)
+            {
+                result[moves[i]] = true;
+            }
+
+            var mvs = new List<int>();
+            for (int i = 0; i < end; i++)
+            {
+                if (result[i])
+                {
+                    mvs.Add(i);
+                    continue;
+                }
+                if (mvs.Any(x => !result[i - x]))
+                {
+                    result[i] = true;
+                }
+            }
+
+            var sum = 0;
+            for (int i = start; i < end; i++)
+            {
+                if (result[i])
+                {
+                    sum++;
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
