d2b91cfa383622a1dc5d9a61de0d0e3906545248
Rosen Urkov <rosen.urkov@gmail.com>
2017-09-01 21:36:30 +0300

    Added some exam 2014 tasks

---
+++ DSA/ExamPreparations/Exam2014/GirlsGoneWild/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirlsGoneWild
+{
+    class Program
+    {
+        static SortedSet<string> result = new SortedSet<string>();
+        static HashSet<string> usedPairs = new HashSet<string>();
+
+        static int shirts = 0;
+        static string skirts = "";
+        static int girlsCount = 0;
+
+        static string[] girls;
+
+        static bool[] usedShirts;
+        static bool[] usedSkirts;
+
+        static void Main(string[] args)
+        {
+            shirts = int.Parse(Console.ReadLine());
+            skirts = Console.ReadLine();
+            girlsCount = int.Parse(Console.ReadLine());
+
+            girls = new string[girlsCount];
+
+            usedShirts = new bool[shirts];
+            usedSkirts = new bool[skirts.Length];
+
+            Recursion(0);
+
+            Console.WriteLine(result.Count);
+            Console.WriteLine(string.Join("\n", result));
+        }
+
+        static void Recursion(int current)
+        {
+            if (current == girlsCount)
+            {
+                result.Add(string.Join("-", girls));
+                return;
+            }
+
+            for (int i = 0; i < shirts; i++)
+            {
+                if (usedShirts[i])
+                {
+                    continue;
+                }
+
+                usedShirts[i] = true;
+                for (int j = 0; j < skirts.Length; j++)
+                {
+                    if (usedSkirts[j])
+                    {
+                        continue;
+                    }
+                    
+                    var pair = i.ToString() + j.ToString();
+                    if (current == 0)
+                    {
+                        usedPairs.Add(pair);
+                    }
+                    else
+                    {
+                        if (usedPairs.Contains(pair))
+                        {
+                            continue;
+                        }
+                    }
+
+                    usedSkirts[j] = true;
+
+                    girls[current] = i.ToString() + skirts[j];
+                    Recursion(current + 1);
+
+                    usedSkirts[j] = false;
+                }
+
+                usedShirts[i] = false;
+            }
+        }
+    }
+}
---
+++ DSA/ExamPreparations/Exam2014/Portals/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portals
+{
+    class Portal
+    {
+        public Portal(int x, int y, int value)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = value;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Value { get; set; }
+    }
+
+    class Program
+    {
+        static int[,] matrix;
+        static bool[,] visited;
+
+        static long bigResult = int.MinValue;
+
+        static void Main(string[] args)
+        {
+            var start = Console.ReadLine().Split(' ');
+            var x = int.Parse(start[0]);
+            var y = int.Parse(start[1]);
+
+            var rowsCols = Console.ReadLine().Split(' ');
+            var rows = int.Parse(rowsCols[0]);
+            var cols = int.Parse(rowsCols[1]);
+
+            matrix = new int[rows, cols];
+            visited = new bool[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var rowVal = Console.ReadLine().Split(' ');
+                for (int col = 0; col < cols; col++)
+                {
+                    if (rowVal[col] == "#")
+                    {
+                        visited[row, col] = true;
+                        matrix[row, col] = int.MinValue;
+                    }
+                    else
+                    {
+                        var value = int.Parse(rowVal[col]);
+                        if (row + value >= matrix.GetLength(0) &&
+                            (row - value < 0 || matrix[row - value, col] == int.MinValue) &&
+                            (col + value >= matrix.GetLength(1) || rowVal[col+value] == "#") &&
+                            (col - value < 0 || matrix[row, col - value] == int.MinValue))
+                        {
+                            visited[row, col] = true;
+                        }
+
+                        matrix[row, col] = value;
+
+                    }
+                }
+            }
+
+            DFS(x, y, 0);
+            Console.WriteLine(bigResult);
+        }
+
+        static void DFS(int x, int y, int result)
+        {
+            if (x < 0 || x >= matrix.GetLength(0) ||
+                y < 0 || y >= matrix.GetLength(1) ||
+                visited[x, y])
+            {
+                if (result > bigResult)
+                {
+                    bigResult = result;
+                }
+
+                return;
+            }
+
+            result += matrix[x, y];
+            visited[x, y] = true;
+
+            DFS(x - matrix[x, y], y, result);
+            DFS(x, y - matrix[x, y], result);
+            DFS(x + matrix[x, y], y, result);
+            DFS(x, y + matrix[x, y], result);
+
+            visited[x, y] = false;
+        }
+    }
+}
