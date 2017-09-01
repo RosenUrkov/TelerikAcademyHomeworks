27f68b93c6d18940e6e93dfef28487fed3bc939b
Rosen Urkov <rosen.urkov@gmail.com>
2017-09-01 14:29:29 +0300

    Added finished exam 2015

--- DSA/ExamPreparations/Exam2015/UnitsOfWork/Program.cs
+++ DSA/ExamPreparations/Exam2015/UnitsOfWork/Program.cs
@@ -3,14 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wintellect.PowerCollections;
 
 namespace UnitsOfWork
 {
+    class Unit : IComparable<Unit>
+    {
+        public Unit(string name, string type, int attack)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Attack = attack;
+        }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Attack { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name + "[" + this.Type + "](" + this.Attack + ")";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Unit;
+            return this.Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public int CompareTo(Unit other)
+        {
+            var comparer = other.Attack.CompareTo(this.Attack);
+            if (comparer == 0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+
+            return comparer;
+        }
+    }
+
     class Program
     {
+        static SortedSet<Unit> units = new SortedSet<Unit>();
+        static Dictionary<string, Unit> nameType = new Dictionary<string, Unit>();
+        static Dictionary<string, SortedSet<Unit>> dict = new Dictionary<string, SortedSet<Unit>>();
+
         static void Main(string[] args)
         {
+            var builder = new StringBuilder();
+            while (true)
+            {
+                var command = Console.ReadLine().Split(' ');
+                switch (command[0][0])
+                {
+                    case 'a': builder.AppendLine(Add(command[1], command[2], int.Parse(command[3]))); break;
+                    case 'r': builder.AppendLine(Remove(command[1])); break;
+                    case 'f': builder.AppendLine(Find(command[1])); break;
+                    case 'p': builder.AppendLine(Power(int.Parse(command[1]))); break;
+                    case 'e': Console.WriteLine(builder.ToString().TrimEnd()); return;
+                }
+            }
+        }
 
+        static string Add(string name, string type, int attack)
+        {
+            var unit = new Unit(name, type, attack);
+            if (units.Add(unit))
+            {
+                if (!dict.ContainsKey(type))
+                {
+                    dict[type] = new SortedSet<Unit>();
+                }
+
+                dict[type].Add(unit);
+                nameType[name] = unit;
+
+                return "SUCCESS: " + name + " added!";
+            }
+
+            return "FAIL: " + name + " already exists!";
+        }
+
+        static string Remove(string name)
+        {
+            if (!nameType.ContainsKey(name))
+            {
+                return "FAIL: " + name + " could not be found!";
+            }
+
+            var unit = nameType[name];
+            if (units.Remove(unit))
+            {
+                dict[unit.Type].Remove(unit);
+                nameType[name] = null;
+                return "SUCCESS: " + name + " removed!";
+            }
+
+            return "FAIL: " + name + " could not be found!";
+        }
+
+        static string Find(string type)
+        {
+            var result = "RESULT: ";
+            if (!dict.ContainsKey(type))
+            {
+                return result;
+            }
+
+            return result + string.Join(", ", dict[type].Take(10));
+        }
+
+        static string Power(int count)
+        {
+            var result = "RESULT: ";
+            var curr = string.Join(", ", units.Take(count));
+
+            return result + curr;
         }
     }
 }
---
+++ DSA/ExamPreparations/Exam2015/Words/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace KMP
+{
+    class Program
+    {
+        static void PrintMatch(int index, string pattern)
+        {
+            for (int i = 0; i < index; ++i)
+            {
+                Console.Write(" ");
+            }
+
+            Console.WriteLine(pattern);
+        }
+
+        static void Main()
+        {
+            var pattern = Console.ReadLine();
+            var text = Console.ReadLine();
+
+            var result = 0;
+            var failLink = PreComputeKMP(pattern);
+
+            int j = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                while (j >= 0 && pattern[j] != text[i])
+                {
+                    j = failLink[j];
+                }
+
+                ++j;
+                if (j == pattern.Length)
+                {
+                    result++;
+                    j = failLink[j];
+                }
+            }
+
+            for (int k = 1; k < pattern.Length; k++)
+            {
+                var prefix = pattern.Substring(0, k);
+                var prefFailLink = PreComputeKMP(prefix);
+
+                var suffix = pattern.Substring(k, pattern.Length - k);
+                var suffFailLink = PreComputeKMP(suffix);
+
+                var prefRes = 0;
+                var suffRes = 0;
+
+                int p = 0;
+                int s = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    while (p >= 0 && prefix[p] != text[i])
+                    {
+                        p = prefFailLink[p];
+                    }
+                    while (s >= 0 && suffix[s] != text[i])
+                    {
+                        s = suffFailLink[s];
+                    }
+
+                    ++p;
+                    ++s;
+
+                    if (p == prefix.Length)
+                    {
+                        prefRes++;
+                        p = prefFailLink[p];
+                    }
+                    if (s == suffix.Length)
+                    {
+                        suffRes++;
+                        s = suffFailLink[s];
+                    }
+                }
+
+                result += (prefRes * suffRes);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static int[] PreComputeKMP(string str)
+        {
+            var failLink = new int[str.Length + 1];
+            failLink[0] = -1;
+            failLink[1] = 0;
+            for (int i = 1; i < str.Length; ++i)
+            {
+                int j = failLink[i];
+                while (j >= 0 && str[i] != str[j])
+                {
+                    j = failLink[j];
+                }
+
+                failLink[i + 1] = j + 1;
+            }
+
+            return failLink;
+        }
+    }
+}
