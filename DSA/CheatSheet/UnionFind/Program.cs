1049f6279d022662771282f9dd43d180ce51ea15
Rosen Urkov <rosen.urkov@gmail.com>
2017-09-03 11:29:57 +0300

    Fixed union find implementation and deleted union and intersect

--- DSA/CheatSheet/UnionFind/Program.cs
+++ DSA/CheatSheet/UnionFind/Program.cs
@@ -1,60 +1,41 @@
 ﻿using System;
+using System.Linq;
 
 namespace UnionFindStructure
 {
     public class UnionFind
     {
-        private int[] array;
+        static int[] array;
 
-        public UnionFind(int n)
+        static void Main()
         {
-            array = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                array[i] = -1;
-            }
-        }
+            var n = int.Parse(Console.ReadLine());
+            array = Enumerable.Repeat(-1, n).ToArray();
+        }             
 
-        public int FindIterative(int x)
-        {
-            while (array[x] >= 0)
-            {
-                x = array[x];
-            }
-            return x;
-        }
-
-        public int Find(int x)
-        {
-            //return array[x] < 0 ? x : array[x] = Find(array[x]);
-            if (array[x] < 0)
-            {
-                return x;
-            }
-            array[x] = Find(array[x]);
-            return array[x];
-        }
-
-        public bool InTheSameSet(int x, int y)
-        {
-            return Find(x) == Find(y);
-        }
-
-        public bool Union(int x, int y)
+        static bool Union(int x, int y)
         {
             x = Find(x);
             y = Find(y);
+
             if (x == y)
             {
                 return false;
             }
+
             array[x] = y;
             return true;
         }
 
-        public void Print()
+        static int Find(int x)
         {
-            Console.WriteLine(string.Join(" ", array));
+            if (array[x] < 0)
+            {
+                return x;
+            }
+
+            array[x] = Find(array[x]);
+            return array[x];
         }
     }
 }
